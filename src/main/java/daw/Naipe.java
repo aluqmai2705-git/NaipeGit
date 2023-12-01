ac473265e75c73b984bd2c0f93d8c4577505b17f
aluqmai2705 <aluqmai2705@g.educaand.es>
2023-12-01 10:07:55 +0100

    Borrado de Enum y array palo creado

--- src/main/java/daw/Naipe.java
+++ src/main/java/daw/Naipe.java
@@ -11,7 +11,25 @@ import java.util.Random;
  * @author alejandro
  */
 public class Naipe {
-    
+    Random random = new Random();
+   private int numeroCarta;
+   private String palo ;
+   private String[] arrayPalos = {"Oro","Basto","Espada","Copa"};
+
+    public static void main(String[] args) {
+        
+        System.out.println(Palo.values());
+    }
+   
+   
+    public Naipe() {
+        this.numeroCarta = random.nextInt(1, 11);;
+        this.palo = arrayPalos[random.nextInt(1, 5)];
+        
+        }  
+        
+        
+   
    
    
    
